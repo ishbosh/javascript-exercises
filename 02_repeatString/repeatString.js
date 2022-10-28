cde4f65c9f3b9148d55033dd3a8547036f329505
ishbosh <99460383+ishbosh@users.noreply.github.com>
2022-10-28 14:40:18 -0700

    Complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num > 0) {
+        let repetition = string;
+    
+        for (let i = 0; i < num - 1; i++) {
+            repetition = repetition + string;
+        }
+        return repetition;
+    } else if (num < 0) {
+        return "ERROR";
+    } else {
+        return "";
+    }
 };
 
 // Do not edit below this line
