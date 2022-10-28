1858c5b3849b6e338954bdf0219cb4674ee91bbc
ishbosh <99460383+ishbosh@users.noreply.github.com>
2022-10-28 15:58:28 -0700

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    charArray = [];
+    for (let i = 0; i <= string.length - 1; i++) {
+        charArray[i] = string.charAt(i);
+    } 
+    return charArray.reverse().join("");
 };
 
 // Do not edit below this line
