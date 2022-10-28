4f057c66885a9747f4278b9b577b45cbf060b823
ishbosh <99460383+ishbosh@users.noreply.github.com>
2022-10-28 16:30:13 -0700

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...values) {
+    for (value of values) {
+        let i = 0;
+        for (item of array) {
+            if (item === value) {
+                array.splice(i, 1);
+            }
+            i++;
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
