0d5622a9ece7390ba3da9be2d0313d7181ab929c
ishbosh <99460383+ishbosh@users.noreply.github.com>
2022-10-28 17:14:12 -0700

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(temperatureInFahrenheit) {
+  // (x-32)*5/9
+  let convertedTemperature = (temperatureInFahrenheit - 32) * (5/9);
+  convertedTemperature = Math.round(convertedTemperature * 10) / 10;
+  return convertedTemperature;
 };
 
-const ctof = function() {
-
+const ctof = function(temperatureInCelsius) {
+  // (x*9/5+32)
+  let convertedTemperature = (temperatureInCelsius * (9/5) + 32);
+  convertedTemperature = Math.round(convertedTemperature * 10) / 10;
+  return convertedTemperature;
 };
 
 // Do not edit below this line
